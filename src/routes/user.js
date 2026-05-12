f0f5eeec329f91e968bd2665a2ecbe2f8993aa2c
yomgolden <yomadeogun@gmail.com>
2026-05-12 15:15:49 +0100

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,1 +1,14 @@
+const router = require("express").Router();
+const User = require("../models/User");
 
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ error: "Not found" });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;

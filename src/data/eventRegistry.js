f00fa823c9a01413b07b6ee975b0248478c7a8c5
yomgolden <yomadeogun@gmail.com>
2026-05-14 10:12:13 +0100

    Update eventRegistry.js

--- src/data/eventRegistry.js
+++ src/data/eventRegistry.js
@@ -1,6 +1,7 @@
 const mushinNightmare = require("../simulation/events/mushinNightmare");
 const blackoutYaba = require("../simulation/events/blackoutYaba");
 const ajegunleWarzone = require("../simulation/events/ajegunleWarzone");
+const evilForest = require("../simulation/events/evilForest");
 
 // ======================================================
 // EVENT REGISTRY
@@ -11,7 +12,9 @@ const REGISTRY = {
 
   blackout_yaba: blackoutYaba,
 
-  ajegunle_warzone: ajegunleWarzone
+  ajegunle_warzone: ajegunleWarzone,
+  
+  evil_forest: evilForest
 };
 
 // ======================================================
@@ -21,7 +24,8 @@ const REGISTRY = {
 const FEATURED = [
   mushinNightmare,
   blackoutYaba,
-  ajegunleWarzone
+  ajegunleWarzone,
+  evilforest
 ];
 
 // ======================================================

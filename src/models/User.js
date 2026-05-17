ba0206f7948e51641cf9bf017d09276fe8ff968c
yomgolden <yomadeogun@gmail.com>
2026-05-17 16:06:15 +0100

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,103 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  telegramId: { type: String, required: true, unique: true },
-  username: { type: String, default: "Survivor" },
-  gold: { type: Number, default: 500 },
-  gems: { type: Number, default: 0 },
-  level: { type: Number, default: 1 },
-  xp: { type: Number, default: 0 },
-  wins: { type: Number, default: 0 },
-  matches: { type: Number, default: 0 },
-  createdAt: { type: Date, default: Date.now },
-  lastActive: { type: Date, default: Date.now }
+
+  telegramId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  username: {
+    type: String,
+    default: "Survivor"
+  },
+
+  firstName: {
+    type: String,
+    default: ""
+  },
+
+  photoUrl: {
+    type: String,
+    default: ""
+  },
+
+  gold: {
+    type: Number,
+    default: 500
+  },
+
+  gems: {
+    type: Number,
+    default: 0
+  },
+
+  level: {
+    type: Number,
+    default: 1
+  },
+
+  xp: {
+    type: Number,
+    default: 0
+  },
+
+  wins: {
+    type: Number,
+    default: 0
+  },
+
+  matches: {
+    type: Number,
+    default: 0
+  },
+
+  /*
+  =====================================
+  RP SYSTEM
+  =====================================
+  */
+
+  weeklyRp: {
+    type: Number,
+    default: 0
+  },
+
+  seasonRp: {
+    type: Number,
+    default: 0
+  },
+
+  weeklyWins: {
+    type: Number,
+    default: 0
+  },
+
+  seasonWins: {
+    type: Number,
+    default: 0
+  },
+
+  rank: {
+    type: String,
+    default: "Rookie"
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+
+  lastActive: {
+    type: Date,
+    default: Date.now
+  }
+
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports =
+  mongoose.model(
+    "User",
+    userSchema
+  );
